61c2ea4016c4129ebdf4f61fbb50333f9acfcfa2
Jociane Costa e Silva <jociane.silva@ambevtech.com.br>
2024-06-19 10:25:34 -0300

    creating an animal management system

--- Exercicio-19-06/Animal.cs
+++ Exercicio-19-06/Animal.cs
@@ -1,12 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Exercicio_19_06
 {
-    internal class Animal
+    public abstract class Animal
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public interface Voar
+    {
+        public void LevantarVoo();
+        public void Pousar();
+
+    }
+
+    public interface Nadar
+    {
+        public int LimiteProfundidade { get; set; }
+        public void Subir(int metros);
+        public void Descer(int metros);
+
+    }
+    public class Anfibio : Animal, Nadar
+    {
+        public int LimiteProfundidade { get => 500; set => throw new NotImplementedException(); }
+
+        public void Descer(int metros)
+        {
+            Console.WriteLine("Para descer " + metros + " O Anfibio usa suas 4 patas. Com um limite de Profundidade: " + LimiteProfundidade);
+        }
+
+        public void Subir(int metros)
+        {
+            Console.WriteLine("Para subir " + metros + " O Anfibio usa suas 4 patas. Com um limite de Profundidade: " + LimiteProfundidade);
+        }
+
+
+    }
+    public class Mamifero : Animal, Nadar
+    {
+        public int LimiteProfundidade { get => 100; set => throw new NotImplementedException(); }
+
+        public void Descer(int metros)
+        {
+            Console.WriteLine("Para descer " + metros + " O Mamifero usa suas 4 patas. Com um limite de Profundidade: " + LimiteProfundidade);
+        }
+
+        public void Subir(int metros)
+        {
+            Console.WriteLine("Para subir " + metros + " O Mamifero usa suas 4 patas. Com um limite de Profundidade: " + LimiteProfundidade);
+        }
+    }
+    public class Aves : Animal, Voar
+    {
+        public void LevantarVoo()
+        {
+            Console.WriteLine("Para Levantar Voo a ave precisa das duas asas");
+        }
+
+        public void Pousar()
+        {
+            Console.WriteLine("Para Pousar a ave precisa das duas asas e suas 2 patinhas");
+        }
+    }
+    public class Peixes : Animal, Nadar
+    {
+        public int LimiteProfundidade { get => 700; set => throw new NotImplementedException(); }
+
+        public void Descer(int metros)
+        {
+            Console.WriteLine("Para descer " + metros + " O Peixe usa suas 2 nadadeiras. Com um limite de Profundidade: " + LimiteProfundidade);
+        }
+
+        public void Subir(int metros)
+        {
+            Console.WriteLine("Para subir " + metros + "O Peixe usa suas 2 nadadeiras. Com um limite de Profundidade: " + LimiteProfundidade);
+        }
+    }
+    public class Repteis : Animal, Nadar
+    {
+        public int LimiteProfundidade { get => 700; set => throw new NotImplementedException(); }
+
+        public void Descer(int metros)
+        {
+            Console.WriteLine("Para descer " + metros + " O Reptil usa suas 4 patas e rabo. Com um limite de Profundidade: " + LimiteProfundidade);
+        }
+
+        public void Subir(int metros)
+        {
+            Console.WriteLine("Para subir " + metros + " O Reptil usa suas 4 patas e rabo. Com um limite de Profundidade: " + LimiteProfundidade);
+        }
     }
 }
--- Exercicio-19-06/Program.cs
+++ Exercicio-19-06/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercicio_19_06;
+
+Anfibio animalAnfibio = new Anfibio();
+animalAnfibio.Descer(10);
+animalAnfibio.Subir(10);
+
+Mamifero animalMamifero = new Mamifero();
+animalMamifero.Descer(10);
+animalMamifero.Subir(10);
+
+Aves animalAve = new Aves();
+animalAve.LevantarVoo();
+animalAve.Pousar();
